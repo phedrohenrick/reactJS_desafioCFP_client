d136ad59d6e4c44f9ca1b293d3090eb837c32555
phedrohenrick <phedrohenrickdasilva@gmail.com>
2025-03-23 15:41:25 -0300

    feat: build together

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ListPaperComponent from './components/ListPaperComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreatePaperComponent from './components/CreatePaperComponent';
+import UpdatePaperComponent from './components/UpdatePaperComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Routes> 
+                          <Route path = "/list" element = {<ListPaperComponent />} />
+                          <Route path = "/Paper" element = {<ListPaperComponent />} /> 
+                          <Route path = "/" element = {<CreatePaperComponent />} />
+                          <Route path = "/update-Paper/:id" element = {<UpdatePaperComponent/>} />
+                    </Routes>
+                </div>
+              <FooterComponent />
+        </Router>
     </div>
   );
 }
